7528f220692d95eecde2aabf4fa41e53e5a3b15d
nicolasjadoul <jadoul.nico@yahoo.fr>
2023-05-04 22:07:57 +0200

    init

---
+++ TouchDown/TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Jadoul Nicolas on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
